4070e16c8e75a2419cd6df61434edff7d7f05542
Nasiri-PC <Nasiri-PC@DESKTOP-0LN6NE5>
2023-10-31 13:40:07 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("enter a number: ");
+int number = int.Parse(Console.ReadLine());
+int fact = 1, n = 1;
+while (n <= number)
+{
+    fact *= n;
+    n++;
+}
+Console.WriteLine("\n{0}! = {1}\n\n", number, fact);
+// پویان نصیری تمرین شماره 5
